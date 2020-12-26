c4c0cf72397301c0a325a5d3dfb855610cf1109c
gagansinghmsitece <gagansingh2016iit@gmail.com>
2020-12-26 10:39:25 +0530

    Adding BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import{Route,Switch} from 'react-router-dom';
+import{BrowserRouter,Route,Switch} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Category from './pages/Category.js';
@@ -12,6 +12,7 @@ import Error from './pages/Error.js';
 function App() {
   return (
     <main>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path="/" component={Home} exact/>
         <Route path="/page/:pagenumber" component={Home} exact/>
@@ -22,6 +23,7 @@ function App() {
         <Route path="/Post/:id" component={MainPost} exact/>
         <Route component={Error}/>
       </Switch>
+      </BrowserRouter/>
     </main>
   );
 }
