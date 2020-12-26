d17635803d5b8b36a7f792737c62566f5dc91867
gagansinghmsitece <gagansingh2016iit@gmail.com>
2020-12-26 09:29:25 +0530

    Updates

---
+++ precache-manifest.737f718be6cf38449044b55b38300e49.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a320d226b0a1955e927201979b563097",
+    "url": "/React-Blog-Frontend/index.html"
+  },
+  {
+    "revision": "4fd8e1ff071ac2ee2afa",
+    "url": "/React-Blog-Frontend/static/css/main.0a4d8631.chunk.css"
+  },
+  {
+    "revision": "c0b27f0387536c26416f",
+    "url": "/React-Blog-Frontend/static/js/2.0c323c25.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/React-Blog-Frontend/static/js/2.0c323c25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fd8e1ff071ac2ee2afa",
+    "url": "/React-Blog-Frontend/static/js/main.a4c1f013.chunk.js"
+  },
+  {
+    "revision": "99b45ed5cc3b87b85ab0",
+    "url": "/React-Blog-Frontend/static/js/runtime-main.c5523889.js"
+  },
+  {
+    "revision": "eed345f9f85f4d2d9d6434d042b74a52",
+    "url": "/React-Blog-Frontend/static/media/banner-bg.eed345f9.jpg"
+  },
+  {
+    "revision": "49a1ccc3c85d7c417c23612725b57391",
+    "url": "/React-Blog-Frontend/static/media/logo.49a1ccc3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Blog-Frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
